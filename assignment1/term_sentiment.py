b27293f24f424ebda7124f81e152472712664d96
Michael Hotan <michael.hotan@gmail.com>
2014-07-09 20:31:03 -0700

    Updates to tweet sentiment

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,13 +1,14 @@
 import sys
+import json
 
 def build_scores_dictionary():
     afinn_file = open("AFINN-111.txt")
-    scores = {} # initialize an empty dictionary
+    scores = {}  # initialize an empty dictionary
     for line in afinn_file:
-        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        term, score = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
         scores[term] = int(score)  # Convert the score to an integer.
 
-    print scores.items()  # Print every (term, score) pair in the dictionary
+    print scores.items()  # Print every (term, score) pair in the dictionary TODO Delete
     return scores
 
 def hw():
@@ -18,9 +19,22 @@ def lines(fp):
 
 def main():
     tweet_file = open(sys.argv[1])  # The first file will should have all the tweets per line
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    # Populate the Sentiment Library
+    scores = build_scores_dictionary()
+
+    # Iterate through each JSON Object
+    for jsonObj in tweet_file.readlines():
+        tweet = json.loads(jsonObj)
+        # Extract the text field from the JSON Object
+        text = tweet['text']
+        print text
+
+    # Separate the words in the text.
+    # Possibly filter word or phrases removing punctuations, Quotations symbols.
+    # add the sentiment score of the word to the ongoing SUM
+    # lines(sent_file)
+    # lines(tweet_file)
 
 if __name__ == '__main__':
     main()
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,41 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def build_scores_dictionary():
+    afinn_file = open("AFINN-111.txt")
+    scores = {}  # initialize an empty dictionary
+    for line in afinn_file:
+        term, score = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    # print scores.items()  # Print every (term, score) pair in the dictionary TODO Delete
+    return scores
 
 def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    tweet_file = open(sys.argv[1])
+
+    # Populate the Sentiment Library
+    scores = build_scores_dictionary()
+
+    # Iterate through each JSON Object
+    for jsonObj in tweet_file.readlines():
+        tweet = json.loads(jsonObj)
+        # Extract the text field from the JSON Object
+        if 'text' not in tweet:  # If there is no text then ignore this tweet.
+            continue
+
+        text = tweet['text']
+        print text + "\n"
+
+        # Separate the words in the text.
+
+    # Possibly filter word or phrases removing punctuations, Quotations symbols.
+    # add the sentiment score of the word to the ongoing SUM
+
+    # lines(tweet_file)
 
 if __name__ == '__main__':
     main()
