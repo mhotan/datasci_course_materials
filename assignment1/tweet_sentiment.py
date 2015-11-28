7691478c995c67606f51d9e5c938abaad4f8d7f6
Michael Hotan <michael.hotan@socrata.com>
2015-11-28 09:50:55 -0800

    Problem 1 complete
    Ignore deliverables folder
    added bin/run to automatically generate output over and over

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -6,12 +6,19 @@ def hw():
 def lines(fp):
     print str(len(fp.readlines()))
 
+
+def scores(sent_file):
+    scores = dict()
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+    return scores
+
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    s = scores(sent_file)
+    print s
 
 if __name__ == '__main__':
     main()
